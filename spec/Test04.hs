c9008455697edc2c314b2adc4b0c00b7c3f92ccc
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2022-11-20 03:26:36 +0000

    finish day 04

--- spec/Test04.hs
+++ spec/Test04.hs
@@ -18,7 +18,7 @@ tests :: TestTree
 tests = Tasty.testGroup "tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = Tasty.testGroup "unit tests" [part1Tests]
+unitTests = Tasty.testGroup "unit tests" [part1Tests, part2Tests]
 
 part1Tests :: TestTree
 part1Tests = Tasty.testGroup "part 1 tests" $ do
@@ -28,6 +28,20 @@ part1Tests = Tasty.testGroup "part 1 tests" $ do
         , ("aa bb cc dd aaa", True) ]
     pure $ HUnit.testCase [qq|example $i|] $
         Day04.valid text @?= valid
+  where
+    shuffle :: Int -> (a, b) -> (Int, a, b)
+    shuffle i (x, y) = (i, x, y)
+
+part2Tests :: TestTree
+part2Tests = Tasty.testGroup "part 2 tests" $ do
+    (i, text, valid) <- zipWith shuffle [1..]
+        [ ("abcde fghij", True)
+        , ("abcde xyz ecdab", False)
+        , ("a ab abc abd abf abj", True)
+        , ("iiii oiii ooii oooi oooo", True)
+        , ("oiii ioii iioi iiio", False) ]
+    pure $ HUnit.testCase [qq|example $i|] $
+        Day04.valid2 text @?= valid
   where
     shuffle :: Int -> (a, b) -> (Int, a, b)
     shuffle i (x, y) = (i, x, y)
--- src/main/Day04.hs
+++ src/main/Day04.hs
@@ -1,9 +1,10 @@
 module Day04 where
 
 import Common
-import Data.Containers.ListUtils (nubOrd)
+import Data.Containers.ListUtils (nubOrd, nubOrdOn)
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.List qualified as List
 import Flow ((.>))
 
 
@@ -18,8 +19,18 @@ valid = Text.words .> distinct
 part1 :: [Text] -> Int
 part1 = filter valid .> length
 
+noAnagrams :: Ord a => [[a]] -> Bool
+noAnagrams xs = xs == nubOrdOn List.sort xs
+
+valid2 :: Text -> Bool
+valid2 = Text.words .> fmap Text.unpack .> noAnagrams
+
+part2 :: [Text] -> Int
+part2 = filter valid2 .> length
+
 main :: IO ()
 main = do
     text <- readInputFileUtf8 "input/day-04.txt"
     let phrases = Text.lines text
     print $ part1 phrases
+    print $ part2 phrases

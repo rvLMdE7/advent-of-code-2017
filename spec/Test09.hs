75d8f140a6cd30f0e7f03ceca6f2f356729d1fe2
rvLMdE7 <75222896+rvLMdE7@users.noreply.github.com>
2022-11-23 19:14:21 +0000

    work on day 9
    Almost there, but our test cases reveal a bug in `noCancelGarbage`:
    If we have a `!` which is itself cancelled by another `!`, as in the
    example `<!!>`, then our naive method of counting `!` goes astray.

--- spec/Test09.hs
+++ spec/Test09.hs
@@ -7,7 +7,7 @@ import Test.Tasty (TestTree)
 import Test.Tasty qualified as Tasty
 import Test.Tasty.HUnit ((@?=))
 import Test.Tasty.HUnit qualified as HUnit
-import Text.InterpolatedString.Perl6 (qq)
+import Text.InterpolatedString.Perl6 (qq, q)
 import Text.Megaparsec qualified as Parse
 
 import Day09 qualified
@@ -20,7 +20,7 @@ tests :: TestTree
 tests = Tasty.testGroup "tests" [unitTests]
 
 unitTests :: TestTree
-unitTests = Tasty.testGroup "unit tests" [part1Tests]
+unitTests = Tasty.testGroup "unit tests" [part1Tests, part2Tests]
 
 part1Tests :: TestTree
 part1Tests = Tasty.testGroup "part 1 tests" $ do
@@ -38,3 +38,19 @@ part1Tests = Tasty.testGroup "part 1 tests" $ do
         , ("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9)
         , ("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9)
         , ("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3) ]
+
+part2Tests :: TestTree
+part2Tests = Tasty.testGroup "part 2 tests" $ do
+    (i, (text, score)) <- zip @Int [1..] examples
+    let parsed = Parse.runParser (Day09.parseGarbage <* Parse.eof) "test" text
+    pure $ HUnit.testCase [qq|example $i|] $
+        second Day09.noCancelGarbage parsed @?= Right score
+  where
+    examples =
+        [ ([q|<>|], 0)
+        , ([q|<random characters>|], 17)
+        , ([q|<<<<>|], 3)
+        , ([q|<{!>}>|], 2)
+        , ([q|<!!>|], 0)
+        , ([q|<!!!>>|], 0)
+        , ([q|<{o"i!a,<{i<a>|], 10) ]
--- src/main/Day09.hs
+++ src/main/Day09.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day09 where
 
 import Common (readInputFileUtf8)
@@ -14,6 +16,7 @@ import System.Exit (die)
 import Text.Megaparsec (Parsec)
 import Text.Megaparsec qualified as Parse
 import Text.Megaparsec.Char qualified as Parse.Char
+import Flow ((.>))
 
 
 type Parser = Parsec Void Text
@@ -45,16 +48,30 @@ parseGarbage = do
     void $ Parse.single '>'
     pure $ MkGarbage $ mconcat garbage
 
+mapSum :: Num b => (a -> b) -> Vector a -> b
+mapSum f = Vec.map f .> Vec.sum
+
 scoreGroups :: Group -> Int
 scoreGroups = go 1
   where
     go :: Int -> Group -> Int
     go level (MkGroup inner) =
         let groups = Vec.mapMaybe (preview _Right) inner
-        in  level + Vec.sum (Vec.map (go (level + 1)) groups)
+        in  level + mapSum (go (level + 1)) groups
+
+noCancelGarbage :: Garbage -> Int
+noCancelGarbage (MkGarbage text) = Text.length text - 2 * Text.count "!" text
+
+noCancelGroup :: Group -> Int
+noCancelGroup (MkGroup inner) =
+    let (garbage, groups) = Vec.partitionWith id inner
+    in  mapSum noCancelGarbage garbage + mapSum noCancelGroup groups
 
 part1 :: Group -> Int
 part1 = scoreGroups
+
+part2 :: Group -> Int
+part2 = noCancelGroup
 
 main :: IO ()
 main = do
@@ -63,5 +80,6 @@ main = do
         Left err -> die $ Parse.errorBundlePretty err
         Right group -> do
             print $ part1 group
+            print $ part2 group
   where
     parse = parseGroup <* Parse.Char.space <* Parse.eof
